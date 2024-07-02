7a7c0e47c1151061220ad113ebbeda51efcbeb5d
Geuni <dlrmsgnl620@naver.com>
2024-07-02 22:36:13 +0900

    execute cli

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import { Button } from 'src/components/ui/button';
+import { PetApiFactory } from './generate';
+
+const petApi = PetApiFactory();
 
 export const App = () => {
+  useEffect(() => {
+    async () => {
+      const { data } = await petApi.getPetById(123);
+    };
+  });
+
   return (
     <div className="absolute left-10 top-0">
       <Button>Click me</Button>
